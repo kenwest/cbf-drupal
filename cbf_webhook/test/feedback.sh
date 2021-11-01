9ae0a5727a85640d0025f44b5d4a93dbc857d5cc
kenwest <west.ken.au@gmail.com>
2021-11-01 15:55:17 +1100

    First functional version of the Feedback Form webhook

---
+++ cbf_webhook/test/feedback.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DIRECTORY=$(dirname $0)
+COMMAND=$(basename $0 .sh)
+
+case $# in
+  ( 1 )
+    DATA=$(ls $DIRECTORY/$COMMAND.$1.*.json)
+    ;;
+  ( * )
+    echo
+    echo Usage ":" bash $COMMAND.sh TEST
+    echo
+    echo TEST is one of the following options ...
+    for i in $(ls $DIRECTORY/$COMMAND.*.json)
+    do
+      echo "    " $(basename $i .json | cut --delimiter=\. --output-delimiter=' ' --fields=2-3 - | tr '-' ' ')
+    done
+    echo
+    exit
+    ;;
+esac
+
+case $1 in
+  ( '01' )
+    HEADERNAME=Dummy
+    HEADERVALUE=dummy
+    ;;
+  ( '02' )
+    HEADERNAME=Webhook-Agent
+    HEADERVALUE=unknown-agent
+    ;;
+  ( * )
+    HEADERNAME=WebhookAgent
+    HEADERVALUE=SC5izvuvijKFYo8d7smlP0X6
+    ;;
+esac
+
+echo
+
+curl \
+  --insecure \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "$HEADERNAME: $HEADERVALUE" \
+  --data @$DATA \
+  --fail \
+    https://citybibleforum.test/webhook/$COMMAND
+
+echo Exit code is $?
+
+cat $DIRECTORY/$COMMAND.$1.*.txt
